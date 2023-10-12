c0d73418b4c7dd73b331c76c8300d250c57712f0
homarortiz <homarjortiz@gmail.com>
2023-10-11 22:21:52 -0400

    PyBank done

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,7 +16,7 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         months.append(row[0])
         profitloss.append(row[1])
-#convert strings to integers 
+#convert strings to integers using list comprehension
 numbers = [int(i) for i in profitloss]
 #print the total amount of months as reference
 print("The total amount of months in the first column is",len(numbers), "months")
@@ -29,11 +29,33 @@ lastmonth = numbers[85]
 firstmonth = numbers[0]
 change = lastmonth - firstmonth
 averagechange = change/85
-#test print
-print("Total Months:",len(months))
-print(" Total:$", sum)
-print("Average Change: $", round(averagechange,2))
-#findgreatesincrease/decrease
-greatestincrease = max(numbers)
-greatestdecrease = min(numbers)
 
+#zip lists to find out which month corresponds to the greatest incease/decrease
+together = list(zip(numbers,months))
+
+#find out the greatest increase/decrease
+greatestincrease = max(together)
+greatestdecrease = min(together)
+
+
+#test print; where the information was obtained from
+
+#print("Total Months:",len(months))
+#print("Total:$", sum)
+#print("Average Change: $", round(averagechange,2))
+#print(greatestincrease)
+#rint(greatestdecrease)
+
+file = open("PyBank Results.txt", "w+")
+
+file.write("Financial Analysis\n")
+
+file.write("\n-------------------------------------\n")
+
+file.write("\nTotal Months: 86\n")
+file.write("\nTotal: $22564198\n")
+file.write("\nAverage Change: $-8311.11\n")
+file.write("\nGreatest Increase in Profits: Mar-13 $1141840\n")
+file.write("\nGreates Decrerase in Profits: Mar-10 -1194133\n")
+           
+file.close()
